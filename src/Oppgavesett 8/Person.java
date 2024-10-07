e82c2ea7fc74c59517e08a75ebf7999b22717ecd
Edvard Klavenes <edvardklavenes@gmail.com>
2024-10-07 13:00:16 +0200

    Start: Person klasse
    Immutable variables with getters

--- src/Oppgavesett 8/Person.java
+++ src/Oppgavesett 8/Person.java
@@ -1,9 +1,35 @@
-
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Person {
 
-    String fornavn;
-    String etternavn;
+    final String firstname;
+    final String surname;
+    final LocalTime birthday;
 
+    //Constructor to make the person
+    public Person(String firstname, String surname, LocalTime birthday){
+        this.firstname = firstname;
+        this.surname = surname;
+        this.birthday = birthday;
+    }
+
+    //Getter methods
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public LocalTime getBirthday(){
+        return birthday;
+    }
+
+    public String getFormattedBirthday(){
+        DateTimeFormatter formatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return birthday.format(formatObj);
+    }
     
 }
